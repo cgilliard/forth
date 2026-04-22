85e924b12b41a6b340a79a3b0063ff3ecc76b59b
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-22 14:52:06 -0700

    clean up build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,7 +13,7 @@ run() {
 		disk_args="-drive file=$2,format=raw,if=none,id=hd0 -device virtio-blk-device,drive=hd0"
 		shift 2
 	fi
-	[ $# -gt 0 ] && echo "Compiling $*" >&2
+	[ $# -gt 0 ] && echo "Building $*" >&2
 	([ $# -gt 0 ] && cat "$@"; printf '\004') | qemu-system-riscv32 \
 		-machine virt -cpu "$CPU" \
 		-nographic -bios none \
@@ -21,17 +21,13 @@ run() {
 		$disk_args
 }
 
-# Build bootstrap tool chain
 run fam0.seed src/fam0.fam0 > bin/fam0
 cmp ./bin/fam0 ./fam0.seed || { echo "fam0: binaries don't match!"; exit 1; }
-
 run bin/fam0 src/fam1.fam0 > bin/fam1
 run bin/fam1 src/fam2.fam1 > bin/fam2
 run bin/fam2 src/fam3.fam2 > bin/fam3
 run bin/fam3 src/forth.fam3 > bin/forth
 run bin/fam3 src/build_full_node.fam3 > bin/build_full_node
-
-# Build full_node (unpatched)
 run bin/forth src/full_node.forth > bin/full_node
 
 # Prepare work disk: full_node (sector-padded) | bible (sector-padded) | output space
@@ -57,8 +53,6 @@ run bin/build_full_node --disk tmp/work.img tmp/sizes.txt > src/tabernacle_confi
 OUT_SECTOR=$((FN_SECTORS + BD_SECTORS))
 dd if=tmp/work.img of=bin/full_node bs=1 skip=$((OUT_SECTOR * 512)) count=$FINAL_SIZE 2>/dev/null
 echo "Patched bin/full_node ($FINAL_SIZE bytes)" >&2
-
-# Build tabernacle
 run bin/fam3 src/tabernacle_config.inc src/tabernacle.fam3 > bin/tabernacle
 
 echo "Success!"
