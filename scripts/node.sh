cc008a0baf248e9b44dacdacc81b5062021f7de7
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-04-23 18:43:34 -0700

    update script

--- scripts/node.sh
+++ scripts/node.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-printf '1234 10000 192.9.152.120:3737 159.54.189.110:3737\004' | q32 bin/tabernacle --net --disk=./tmp/disk.img
+printf '3737 10000 192.9.152.120:3737 159.54.189.110:3737\004' | \
+	q32 bin/tabernacle \
+	--hostfwd=udp::3737-:3737 \
+	--net \
+	--disk=./tmp/disk.img
